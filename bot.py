a0873b4cd5bf4b91d7437fb75c01059a07613a78
Lucas <lucascrespibarbosa@gmail.com>
2026-08-19 16:46:29 -0300

    new feature

--- bot.py
+++ bot.py
@@ -8,10 +8,18 @@ bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
 bot.set_my_name("Imaculado")
 
+@bot.message_handler()
+def fallback(message: telebot.types.Message):
+    try:
+        bot.set_message_reaction(message.chat.id, message.id, [telebot.types.ReactionTypeEmoji("❤️")])
+    except:
+        bot.reply_to(message, "Sorry, I couldn't process that.")
+
+
 @bot.message_handler(commands=['repeat'])
 def repeat_message(message: telebot.types.Message):
     text_to_repeat = message.text.split(' ', 1)
-    
+
     if len(text_to_repeat) > 1:
         bot.reply_to(message, text_to_repeat[1])
     else:
